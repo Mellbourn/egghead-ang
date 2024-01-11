d5b421586987bc6c812b52d4ea035b06b851d295
Klas Mellbourn <klas@mellbourn.net>
2024-01-11 10:44:19 +0100

    inline template and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,8 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: '<h1>{{title}}</h1>',
+  styles: 'h1 { color: blue }',
 })
 export class AppComponent {
   title = 'egghead-angular';
